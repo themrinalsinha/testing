62974f4f3c5c75736ded667288e794572ef2d2a8
Mrinal Sinha <themrinalsinha@gmail.com>
2019-07-30 23:54:42 +0530

    another way to write

--- programming_knowledge_pytest/test_math.py
+++ programming_knowledge_pytest/test_math.py
@@ -1,13 +1,20 @@
 import pytest
 import math_func
 
-def test_add():
+# def test_add():
+#     assert math_func.add(7, 3) == 10
+
+# def test_add_strings():
+#     result = math_func.add('Hello ', 'world')
+#     assert result == 'Hello world'
+
+# def test_add_float():
+#     result = math_func.add(10.5, 25.5)
+#     assert result == 36
+
+# OR
+
+def test__add():
     assert math_func.add(7, 3) == 10
-
-def test_add_strings():
-    result = math_func.add('Hello ', 'world')
-    assert result == 'Hello world'
-
-def test_add_float():
-    result = math_func.add(10.5, 25.5)
-    assert result == 36
+    assert math_func.add('Hello ', 'world') == 'Hello world'
+    assert math_func.add(10.5, 25.5) == 36
