df4e42a55faeaf0e379bf4fd75ca3be19fd40acd
Mrinal Sinha <themrinalsinha@gmail.com>
2019-07-30 23:59:30 +0530

    using parameterized marker

--- programming_knowledge_pytest/test_math.py
+++ programming_knowledge_pytest/test_math.py
@@ -14,7 +14,17 @@ import math_func
 
 # OR
 
-def test__add():
-    assert math_func.add(7, 3) == 10
-    assert math_func.add('Hello ', 'world') == 'Hello world'
-    assert math_func.add(10.5, 25.5) == 36
+# def test__add():
+#     assert math_func.add(7, 3) == 10
+#     assert math_func.add('Hello ', 'world') == 'Hello world'
+#     assert math_func.add(10.5, 25.5) == 36
+
+# OR
+
+@pytest.mark.parametrize('x, y, res', [
+    (7, 3, 10),
+    ('hello', ' world', 'hello world'),
+    (10.5, 25.5, 36)
+])
+def test_add(x, y, res):
+    assert math_func.add(x, y) == res
