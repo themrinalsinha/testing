96b2d9192c0a6b82f2182a8a09f2d58e07435493
Mrinal Sinha <themrinalsinha@gmail.com>
2019-07-30 23:27:08 +0530

    running with marker

--- programming_knowledge_pytest/test_math_func.py
+++ programming_knowledge_pytest/test_math_func.py
@@ -1,21 +1,26 @@
 import math_func
+import pytest
 
+@pytest.mark.number
 def test_add():
     assert math_func.add(7, 3) == 10
     assert math_func.add(7, 1) == 8
     assert math_func.add(7)    == 9
 
+@pytest.mark.number
 def test_product():
     assert math_func.product(7, 10) == 70
     assert math_func.product(7)     == 14
     # assert math_func.product(7)     == 12
 
+@pytest.mark.strings
 def test_add_string():
     result = math_func.add('hello ', 'world')
     assert result == 'hello world'
     assert type(result) is str
     assert 'helio' not in result
 
+@pytest.mark.strings
 def test_product_string():
     assert math_func.product('hello ', 3) == 'hello hello hello '
     result = math_func.product('hello ')
@@ -34,3 +39,8 @@ def test_product_string():
 # $ pytest -v -k "add"                   # function havig add in name
 # $ pytest -v -k "add or string"         # function having add or string in name
 # $ pytest -v -k "add and string"        # function having add and string in name
+
+
+# Running tests with marker ('-m') option
+# $ pytest -v -m number
+# $ pytest -v -m strings
