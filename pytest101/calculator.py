28abdbcca255d588094c0bf49fba74dd26eb8dad
Mrinal Sinha <themrinalsinha@gmail.com>
2019-07-30 22:32:43 +0530

    handling exceptions

--- pytest101/calculator.py
+++ pytest101/calculator.py
@@ -1,6 +1,13 @@
+
+class CalculatorError(Exception):
+    pass
+
 class Calculator(object):
     def add(self, a, b):
-        return a + b
+        try:
+            return a + b
+        except TypeError:
+            raise CalculatorError('Invalid value')
 
     def subtract(self, a, b):
         return a - b
--- pytest101/test_calculator.py
+++ pytest101/test_calculator.py
@@ -1,4 +1,5 @@
-from calculator import Calculator
+import pytest
+from calculator import Calculator, CalculatorError
 
 def test_add():
     calculator = Calculator()
@@ -19,3 +20,9 @@ def test_division():
     calculator = Calculator()
     result     = calculator.division(10, 2)
     assert result == 5
+
+def test_add_weird_stuff():
+    calculator = Calculator()
+    with pytest.raises(CalculatorError):
+        result = calculator.add('two', 3)
+    # assert result == 5
