9296433832cf7f2ec0fc815927ca92f29c49d33c
Mrinal Sinha <themrinalsinha@gmail.com>
2019-07-31 00:18:51 +0530

    setup module method

--- programming_knowledge_pytest/test_student.py
+++ programming_knowledge_pytest/test_student.py
@@ -1,18 +1,30 @@
 from student_db import StudentDB
 import pytest
 
-def test_scott_data():
+# Using setup and teardown method
+db = None
+def setup_module(module):
+    global db
     db = StudentDB()
     db.connect('data.json')
+
+def test_scott_data():
+    # db = StudentDB()
+    # db.connect('data.json')
     scott_data = db.get_data('Scott')
     assert scott_data['id'] == 1
     assert scott_data['name'] == 'Scott'
     assert scott_data['result'] == 'pass'
 
 def test_mark_data():
-    db = StudentDB()
-    db.connect('data.json')
+    # db = StudentDB()
+    # db.connect('data.json')
     scott_data = db.get_data('Mark')
     assert scott_data['id'] == 2
     assert scott_data['name'] == 'Mark'
     assert scott_data['result'] == 'fail'
+
+# as you can see above, there is alot of code repeation here,
+# we are also initializing db alot of times, which is high resource intensive
+
+# In order to first solve it, we'll use setup and tear down method.
